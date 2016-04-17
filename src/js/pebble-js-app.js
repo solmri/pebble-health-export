4c9f3f2466c40fbaf75f41897de2801a37b5a10e
Natasha Kerensikova <natacha@instinctive.eu>
2016-04-17 21:43:52 +0000

    Add configuration variables for payload signature

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -18,6 +18,11 @@ var cfg_endpoint = null;
 var cfg_data_field = null;
 var cfg_bundle_max = 1;
 var cfg_bundle_separator = "\r\n";
+var cfg_sign_algo = "";
+var cfg_sign_field = "";
+var cfg_sign_field_format = "";
+var cfg_sign_key = "";
+var cfg_sign_key_format = "";
 
 var to_send = [];
 var senders = [new XMLHttpRequest(), new XMLHttpRequest()];
@@ -80,6 +85,11 @@ Pebble.addEventListener("ready", function() {
    cfg_data_field = localStorage.getItem("cfgDataField");
    cfg_bundle_max = parseInt(localStorage.getItem("cfgBundleMax") || "1", 10);
    cfg_bundle_separator = localStorage.getItem("cfgBundleSeparator");
+   cfg_sign_algo = localStorage.getItem("cfgSignAlgorithm");
+   cfg_sign_field = localStorage.getItem("cfgSignFieldName");
+   cfg_sign_field_format = localStorage.getItem("cfgSignFieldFormat");
+   cfg_sign_key = localStorage.getItem("cfgSignKey");
+   cfg_sign_key_format = localStorage.getItem("cfgSignKeyFormat");
 
    if (!(cfg_bundle_max >= 1)) cfg_bundle_max = 1;
 
@@ -123,6 +133,13 @@ Pebble.addEventListener("showConfiguration", function() {
    if (cfg_bundle_separator) {
       settings += "&bundle_sep=" + encodeURIComponent(cfg_bundle_separator);
    }
+   if (cfg_sign_field) {
+      settings += "&s_algo=" + encodeURIComponent(cfg_sign_algo)
+       + "&s_field=" + encodeURIComponent(cfg_sign_field)
+       + "&s_fieldf=" + encodeURIComponent(cfg_sign_field_format)
+       + "&s_key=" + encodeURIComponent(cfg_sign_key)
+       + "&s_keyf=" + encodeURIComponent(cfg_sign_key_format);
+   }
 
    Pebble.openURL("https://cdn.rawgit.com/faelys/pebble-health-export/v1.0/config.html" + settings);
 });
@@ -151,6 +168,33 @@ Pebble.addEventListener("webviewclosed", function(e) {
       cfg_bundle_separator = decodeURIComponent(configData.bundleSeparator);
       localStorage.setItem("cfgBundleSeparator", cfg_bundle_separator);
    }
+
+   if (configData.signAlgorithm) {
+      cfg_sign_algo = configData.signAlgorithm;
+      localStorage.setItem("cfgSignAlgorithm", cfg_sign_algo);
+   }
+
+   if (configData.signFieldName) {
+      cfg_sign_field = configData.signFieldName;
+      localStorage.setItem("cfgSignFieldName", cfg_sign_field);
+   }
+
+   if (configData.signFieldFormat) {
+      cfg_sign_field_format = configData.signFieldFormat;
+      localStorage.setItem("cfgSignFieldFormat", cfg_sign_field_format);
+   }
+
+   if (configData.signKey) {
+      cfg_sign_key = configData.signKey;
+      localStorage.setItem("cfgSignKey", cfg_sign_key);
+   }
+
+   if (configData.signKeyFormat) {
+      cfg_sign_key_format = configData.signKeyFormat;
+      localStorage.setItem("cfgSignKeyFormat", cfg_sign_key_format);
+   }
+
+   console.log(cfg_sign_field ? "Signature enabled" : "Signature disabled");
 
    if (configData.resend) {
       senders[0].abort();
